9087173622a6f5be46d8fca0fdb9340d376c5c3b
Eduardo Ruiz <ruizzmarin20@gmail.com>
2025-03-05 19:33:00 -0300

    linting failures

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,15 @@
 class UsersController < ApplicationController
-    before_action :set_user, only: [:show, :update, :destroy]
-  
+    before_action :set_user, only: [ :show, :update, :destroy ]
+
     def index
       @users = User.all
       render json: @users
     end
-  
+
     def show
       render json: @user
     end
-  
+
     def create
       @user = User.new(user_params)
       if @user.save
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
         render json: @user.errors, status: :unprocessable_entity
       end
     end
-  
+
     def update
       if @user.update(user_params)
         render json: @user
@@ -26,22 +26,21 @@ class UsersController < ApplicationController
         render json: @user.errors, status: :unprocessable_entity
       end
     end
-  
+
     def destroy
       @user.destroy
       head :no_content
     end
-  
+
     private
-  
+
     def set_user
       @user = User.find(params[:id])
     rescue ActiveRecord::RecordNotFound
-      render json: { error: 'User not found' }, status: :not_found
+      render json: { error: "User not found" }, status: :not_found
     end
-  
+
     def user_params
       params.require(:user).permit(:name, :email, :password)
     end
-  end
-  
+end
